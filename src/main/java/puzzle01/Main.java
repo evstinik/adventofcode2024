d7af9ff5cdb105ec79b4381249a47b99dec543af
Nikita Evstigneev <nikita.evstigneev@saboit.de>
2024-12-01 08:54:36 +0100

    Puzzle 1 solution

--- src/main/java/puzzle01/Main.java
+++ src/main/java/puzzle01/Main.java
@@ -1,11 +1,60 @@
 package puzzle01;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(getHelloWorld());
+    List<Integer> leftList = new ArrayList<>();
+    List<Integer> rightList = new ArrayList<>();
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        var main = new Main();
+        if (args.length == 0) {
+            System.out.println("Please provide an input file");
+            return;
+        }
+        var answer = main.solve(args[1]);
+        System.out.printf("Answer: %d", answer);
     }
 
-    public static String getHelloWorld() {
-        return "Hello world!";
+    public int solve(String inputFile) throws URISyntaxException, IOException {
+        readInput(inputFile);
+        return solvePuzzle();
     }
+
+    private void readInput(String inputFile) throws URISyntaxException, IOException {
+        var resource = Main.class.getClassLoader().getResource(inputFile);
+        assert resource != null;
+        var path = Paths.get(resource.toURI());
+        var scanner = new Scanner(path);
+
+        while (scanner.hasNext()) {
+            var leftItem = scanner.nextInt();
+            leftList.add(leftItem);
+
+            var rightItem = scanner.nextInt();
+            rightList.add(rightItem);
+        }
+
+        scanner.close();
+    }
+
+    private int solvePuzzle() {
+        leftList.sort(Comparator.naturalOrder());
+        rightList.sort(Comparator.naturalOrder());
+
+        var totalDistance = 0;
+        for (int i = 0; i < leftList.size(); i++) {
+            var distance = Math.abs(leftList.get(i) - rightList.get(i));
+            totalDistance += distance;
+        }
+
+        return totalDistance;
+    }
+
 }
--- src/test/java/puzzle01/MainTest.java
+++ src/test/java/puzzle01/MainTest.java
@@ -2,13 +2,22 @@ package puzzle01;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @Test
-    void shouldReturnCorrectHelloWorld() {
-        var actual = Main.getHelloWorld();
-        assertEquals("Hello world!", actual);
+    void shouldReturnCorrectResultForInput0() throws URISyntaxException, IOException {
+        var actual = new Main().solve("puzzle01/input0.txt");
+        assertEquals(11, actual);
+    }
+
+    @Test
+    void shouldReturnCorrectResultForInput1() throws URISyntaxException, IOException {
+        var actual = new Main().solve("puzzle01/input1.txt");
+        assertEquals(1320851, actual);
     }
 }
